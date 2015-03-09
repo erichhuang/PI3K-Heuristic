f571581204a661578a4a9e2461cb5ff564336846
Erich S. Huang, MD, PhD <erich@mac.com>
2015-03-09 11:43:27 -0400

    Modified the 'buildPrelimHeuristic()' function to: (1) Deal with breaking API changes in the R Synapse Client; (2) Return intermediate data to the workspace.

--- functions/buildPrelimHeuristic.R
+++ functions/buildPrelimHeuristic.R
@@ -1,9 +1,10 @@
 ## buildPrelimHeuristic.R
 
 ## Erich S. Huang, MD, PhD
-## Sage Bionetworks
-## erich@post.harvard.edu
-## erich.huang@sagebase.org
+## Division of Translational Bioinformatics
+## Department of Biostatistics & Bioinformatics
+## Duke University School of Medicine
+## erich.huang@duke.edu
 
 ## Now build the PIK3CA mutation-based pathway heuristic model
 ## 
@@ -12,15 +13,41 @@ buildPrelimHeuristic <- function(){
   require(randomForest)
   require(synapseClient)
   
-  ## LOAD DATA OBJECTS FROM SYNAPSE
-  mutEnt <- loadEntity('syn1810390')
-  intExEnt <- loadEntity('syn1810391')
+  functionEnv <- new.env()
   
-  mutationDF <- mutEnt$objects$mutationDF
-  intersectExpress <- intExEnt$objects$intersectExpress
+  ## LOAD DATA OBJECTS FROM SYNAPSE
+  ## NOTES ON SYNAPSE CLIENT 1.6-1: Breaking API changes and a total reconfiguration
+  # of the ~/.synapseCache mean that 'legacy' R Objects no longer have a slot with
+  # Synapse entities, and that these R objects are no longer automatically unzipped
+  # therefore I'll create a 'synLegacy()' function that unzips the R Objects and load 
+  # them into memory
+  synLegacy <- function(fileEntity)
+  {
+    require(synapseClient)
+    filePath <- getFileLocation(fileEntity)
+    rObject <- load(unzip(filePath, 
+                          exdir = strsplit(filePath, '/archive.zip')[[1]][1]),
+                    envir = functionEnv)
+    return(rObject)
+  }
+  
+  cat('[] Loading processed data\n')
+  mutEnt <- synGet('syn1810390')
+  intExEnt <- synGet('syn1810391')
+  
+  # Obsolete accessor slots from legacy R client
+  #   mutationDF <- mutEnt$objects$mutationDF
+  #   intersectExpress <- intExEnt$objects$intersectExpress
+  
+  # Use 'synLegacy()' function
+  synLegacy(mutEnt)
+  synLegacy(intExEnt)
+  
+  mutationDF <- functionEnv$mutationDF
+  intersectExpress <- functionEnv$intersectExpress
   
   ## MUTATION EXCLUSIVITY INDICATOR
-  cat('[10] Identifying samples that only have single locus PI3K pathway mutations\n')
+  cat('[] Identifying samples that only have single locus PI3K pathway mutations\n')
   excInd <- apply(mutationDF, 1, sum)
   ## 6 samples have mutations at two of our loci of interest
   exclusivePi3k <- rownames(mutationDF)[mutationDF$pik3ca == 1 & excInd < 2]
@@ -31,12 +58,12 @@ buildPrelimHeuristic <- function(){
   allExc <- c(exclusivePi3k, exclusivePten, exclusivePik3r1, exclusiveAkt)
   
   ## WE WANT TO BUILD THE MODEL ON EXCLUSIVELY PI3K MUTANT PATIENTS VERSUS WT
-  cat('[9] Subsetting the data on WT versus PIK3CA mutant samples\n')
+  cat('[] Subsetting the data on WT versus PIK3CA mutant samples\n')
   wtExpress <- intersectExpress[ , !colnames(intersectExpress) %in% allExc]
   pi3kExpress <- intersectExpress[ , exclusivePi3k]
   
   ## RANDOMLY DIVIDE INTO TRAINING AND VALIDATION COHORTS
-  cat('[11] Dividing the data into training & validation cohorts\n')
+  cat('[] Dividing the data into training & validation cohorts\n')
   set.seed(1212121745)
   wtSampInd <- sample(1:dim(wtExpress)[2], dim(wtExpress)[2]/2)
   pi3kSampInd <- sample(1:dim(pi3kExpress)[2], dim(pi3kExpress)[2]/2)
@@ -51,10 +78,10 @@ buildPrelimHeuristic <- function(){
   validClass <- mutationDF[colnames(validExpress), 1]
   
   ## GENERATE THE NEW RANDOM FOREST MODEL PHASE 1
-  cat('[12] Building prototype PI3K heuristic model\n')
+  cat('[] Building prototype PI3K heuristic model\n')
   pi3kPrelimModel <- randomForest(t(trainExpress),
                                   as.factor(trainClass),
-                                  ntree = 500,
+                                  ntree = 50,
                                   do.trace = 2,
                                   importance = TRUE,
                                   proximity = TRUE)
@@ -68,31 +95,31 @@ buildPrelimHeuristic <- function(){
                          validScoreHat[validNumeric == 1, 2])
   
   ## SENDING INTERMEDIATE OBJECTS UP TO SYNAPSE
-  trainExEnt <- loadEntity('syn1810659')
-  trainExEnt <- addObject(trainExEnt, trainExpress)
-  trainExEnt <- storeEntity(trainExEnt)
+  #   trainExEnt <- loadEntity('syn1810659')
+  #   trainExEnt <- addObject(trainExEnt, trainExpress)
+  #   trainExEnt <- storeEntity(trainExEnt)
+  #   
+  #   validExEnt <- loadEntity('syn1810660')
+  #   validExEnt <- addObject(validExEnt, validExpress)
+  #   validExEnt <- storeEntity(validExEnt)
+  #   
+  #   tClassEnt <- loadEntity('syn1810661')
+  #   tClassEnt <- addObject(tClassEnt, trainClass)
+  #   tClassEnt <- storeEntity(tClassEnt)
+  #   
+  #   vClassEnt <- loadEntity('syn1810662')
+  #   vClassEnt <- addObject(vClassEnt, validClass)
+  #   vClassEnt <- storeEntity(vClassEnt)
+  #   
+  #   modEnt <- loadEntity('syn1810663')
+  #   modEnt <- addObject(modEnt, pi3kPrelimModel)
+  #   modEnt <- storeEntity(modEnt)
+  #   
+  #   vClassHatEnt <- loadEntity('syn1810664')
+  #   vClassHatEnt <- addObject(vClassHatEnt, validScoreHat)
+  #   vClassHatEnt <- storeEntity(vClassHatEnt)
   
-  validExEnt <- loadEntity('syn1810660')
-  validExEnt <- addObject(validExEnt, validExpress)
-  validExEnt <- storeEntity(validExEnt)
-  
-  tClassEnt <- loadEntity('syn1810661')
-  tClassEnt <- addObject(tClassEnt, trainClass)
-  tClassEnt <- storeEntity(tClassEnt)
-  
-  vClassEnt <- loadEntity('syn1810662')
-  vClassEnt <- addObject(vClassEnt, validClass)
-  vClassEnt <- storeEntity(vClassEnt)
-  
-  modEnt <- loadEntity('syn1810663')
-  modEnt <- addObject(modEnt, pi3kPrelimModel)
-  modEnt <- storeEntity(modEnt)
-  
-  vClassHatEnt <- loadEntity('syn1810664')
-  vClassHatEnt <- addObject(vClassHatEnt, validScoreHat)
-  vClassHatEnt <- storeEntity(vClassHatEnt)
-  
-  cat('[14] Returning objects as a list to Workspace\n')
+  cat('[] Returning objects as a list to Workspace\n')
   returnList <- list('trainExpress' = trainExpress,
                      'validExpress' = validExpress,
                      'trainClass' = trainClass,
@@ -100,4 +127,5 @@ buildPrelimHeuristic <- function(){
                      'model' = pi3kPrelimModel,
                      'validScoreHat' = validScoreHat,
                      'rankSum' = rankSum)
+  return(returnList)
 }
